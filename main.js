2c5d2487116cdc578a5fd10316e34b0d17e2b206
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2023-01-10 12:17:01 +0100

    :fire: :art: Display Movies To The DOM :sparkles: :globe_with_meridians:

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=' + api_ke
 
 
 const form = document.getElementById('form');
+const main = document.getElementById('main');
 const search = document.getElementById('search');
 
 getMovies(API_URL)
@@ -14,7 +15,41 @@ getMovies(API_URL)
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.results);
+    displayMovies(data.results);
+}
+
+function displayMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}" />
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassColorByRateVote(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+        </div>
+        `;
+
+        main.appendChild(movieEl);
+    })
+}
+
+function getClassColorByRateVote(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
 }
 
 form.addEventListener('submit', (e) => {
