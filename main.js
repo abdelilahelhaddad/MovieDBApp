9550c2081dc90df8ed58e3c220e2baea9d4fb9eb
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2023-01-10 11:39:17 +0100

    :fire: :art: Fetch Movies With Search Request :sparkles: :globe_with_meridians:

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&query="';
 
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 getMovies(API_URL)
 
 async function getMovies(url) {
@@ -12,3 +16,16 @@ async function getMovies(url) {
     const data = await res.json();
     console.log(data.results);
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm != '') {
+        getMovies(SEARCH_URL + searchTerm);
+        search.value = '';
+    } else {
+        window.location.reload();
+    }
+});
